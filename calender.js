7ed01a52330fa420af0c12991d21e309fc2ca294
M.Raheel <r.loyaldev@gmail.com>
2025-04-14 19:21:12 +0500

    try again

--- calender.js
+++ calender.js
@@ -57,7 +57,7 @@
 //     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 //     if (currMonth < 0 || currMonth > 11) {
 //       // if current month is less than 0 or greater than 11
-//       // creating a new date of current year & month and pass it as date value
+//    ~   // creating a new date of current year & month and pass it as date value
 //       date = new Date(currYear, currMonth, new Date().getDate());
 //       currYear = date.getFullYear(); // updating current year with new date year
 //       currMonth = date.getMonth(); // updating current month with new date month
